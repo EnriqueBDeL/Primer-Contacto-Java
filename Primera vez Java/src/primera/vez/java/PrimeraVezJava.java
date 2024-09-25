6b076cff46706de30b7510ab2c92e385245add06
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-09-25 19:37:57 +0200

    Add files via upload

---
+++ Primera vez Java/src/primera/vez/java/PrimeraVezJava.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primera.vez.java;
+
+/**
+ *
+ * @author enriq
+ */
+public class PrimeraVezJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.printf("\n\nHola Mundo "+ 2024);
+        
+        int a = 15;
+        float b = 27.8f;
+        boolean c = true;
+      
+        System.out.println("\n"+a);
+               System.out.println(b);
+                  System.out.println(c);
+               
+        
+                  
+            //COmentario tipo 1.
+            
+            /* COmentario tipo 2. */
+                  
+                  
+                  
+    }
+          
+}
